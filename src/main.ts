9bfff082c44e670741ab023a4dc66c658478ba11
Alex <alexandepp@gmail.com>
2023-05-07 16:16:58 +0300

    nest start

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { settingsEnv } from "./settings/settings";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(`${settingsEnv.PORT}`);
 }
 
 bootstrap();
