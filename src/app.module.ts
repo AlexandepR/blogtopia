d9ee800b4d4daee1f59cf88f6939fbf62913ffab
Alex <alexandepp@gmail.com>
2023-05-07 17:58:44 +0300

    start nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,25 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { BlogsModule } from "./blogs/blogs.module";
 import { BlogsController } from "./blogs/blogs.controller";
 import { BlogsService } from "./blogs/blogs.service";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { settingsEnv } from "./settings/settings";
+import { join } from "path";
+
+
+
+
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath:join(__dirname, '..', 'swagger-static'),
+      serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger'
+    }),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL),
     BlogsModule,
   ],
-  controllers: [AppController, BlogsController],
-  providers: [AppService, BlogsService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 export const settingsEnv = {
   // const mongoUri : process.env.mongoURI || "mongodb://0.0.0.0:27017";
@@ -15,4 +15,5 @@ export const settingsEnv = {
   JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET as string,
   JWT_TOKEN_LIFE: process.env.JWT_TOKEN_LIFE,
   JWT_REFRESH_TOKEN_LIFE: process.env.JWT_REFRESH_TOKEN_LIFE,
+  NODE_ENV: process.env.NODE_ENV,
 }
