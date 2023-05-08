3e1cdf39119617d5e354083c1c8f70cd89b4bd27
Alex <alexandepp@gmail.com>
2023-05-08 23:33:57 +0300

    add type for blog

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -1,19 +1,54 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from "@nestjs/common";
 import { BlogsService } from "./blogs.service";
+import { CreateBlogInputModelType, PutBlogInputModelType } from "./type/blogsType";
 
 
-@Controller('blogs')
+@Controller("blogs")
 export class BlogsController {
-  constructor(private blogsService: BlogsService) {}
-
+  // constructor(private blogsService: PostsService) {}
+  constructor(protected blogsService: BlogsService) {
+  }
   @Get()
-  findAll() {
-    return   'Happy hacking'
-    // return this.blogsService.findAll()
+  // getBlogs(@Query("term") term: string) {
+  async getBlogs(
+    @Query() query: { term: string }
+  ) {
+    return this.blogsService.findAll(query.term);
+  }
+  @Get(":id")
+  async getBlog(@Param("id") blogId: string) {
+
+  }
+  @Get()
+  async GetPostsByBlog() {
+
+  }
+  @Post("blogs")
+  async createBlog(@Body() dto: CreateBlogInputModelType) {
+    return this.blogsService.create(dto);
+  }
+  // @Post()
+  // createPostForBlog(@Body() inputModel: PutBlogInputModelType ) {
+  //
+  // }
+  @Put(":id")
+  async updateBlog(
+    @Param("id")
+    @Body() model: PutBlogInputModelType) {
+      const blogs = await this.blogsService.findAll('WrongData');
+      const result = blogs.find((b) => b.name === 'a')!;
+
+      result.setAge(2000000000)
+    // return this.blogsService.save({} as CreateBlogInputModelType)
+
+  }
+  @Delete((":id"))
+  async deleteBlog(@Param("id") blogId: string) {
+
+  }
+  @Delete()
+  async deleteAllBlog() {
+
   }
 
-  // @Post()
-  // // createOne(@Body() createBlogDto: CreateBlogDto) {
-  //   return this.blogsService.createOne(createBlogDto);
-  // }
 }
