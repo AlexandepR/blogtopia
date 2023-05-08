96d397fe11c8faecc60a2521aa75af277e9df907
Alex <alexandepp@gmail.com>
2023-05-08 23:33:14 +0300

    add type for blog

---
+++ src/posts/type/postsType.ts
@@ -0,0 +1,9 @@
+// export type CreatePostInputModelType = {
+//   name: string,
+//   description: string,
+//   websiteUrl: string,
+// }
+// export type PutPostInputModelType = {
+//   name: string,
+//   websiteUrl: string,
+// }
