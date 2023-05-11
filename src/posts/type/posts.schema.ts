f6081f27f52729e81758d99ecb71b79f985c418a
Alex <alexandepp@gmail.com>
2023-05-11 14:38:07 +0300

    postSchema

--- src/posts/type/posts.schema.ts
+++ src/posts/type/posts.schema.ts
@@ -1,49 +1,180 @@
-// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-// import mongoose, { HydratedDocument, Types } from "mongoose";
-//
-//
-// @Schema()
-// export class Post {
-//   @Prop({
-//     required:true,
-//     type: mongoose.Schema.Types.ObjectId
-//   })
-//   _id: Types.ObjectId;
-//
-//   @Prop({
-//     required: true,
-//   })
-//   name: string;
-//
-//   @Prop()
-//   age: number;
-//
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument, Model, Types } from "mongoose";
+import { CreatePostInputModelType } from "./postsType";
+import { BlogDocument } from "../../blogs/type/blogs.schema";
+
+
+@Schema()
+
+// export class ExtendedLikesInfoSchema = SchemaFactory(NewestLikes)
+// class NewestLikes {
 //   @Prop({
 //     default: []
 //   })
-//   bread: string
+//   _id: Types.ObjectId;
+//   description: string;
+//   addedAt: string;
+//   userId: string;
+//   login: string;
+// }
 //
+// class PostLikesData {
 //   @Prop({
-//     default: [],
-//     type: [PostSchema]
+//     default: []
 //   })
-//   comments: Posts[]
-//
-//   setAge(newAge: number) {
-//     if (newAge <= 0) throw new Error('Bade age')
-//     this.age = newAge
-//   }
-//   static createSuperPost() {
-//     const createdPost = new this.postModel(createPostDto)
-//     return 'static------'
-//   }
+//   _id: Types.ObjectId;
+//   createdAt: string;
+//   userId: Types.ObjectId;
+//   userLogin: string;
 // }
-//
-// export const BlogSchema = SchemaFactory.createForClass(Post);
-// BlogSchema.methods = {
-//   setAge: Post.prototype.setAge
+// class ExtendedLikesInfo {
+//   @Prop()
+//   likesData: PostLikesData[];
+//   dislikesData: PostLikesData[];
+//   likesCount: number;
+//   dislikesCount: number;
+//   myStatus: "None" | "Like" | "Dislike";
+//   newestLikes: NewestLikes[];
+//   // newestLikes: Array <{
+//   //   _id: Types.ObjectId;
+//   //   description: string;
+//   //   addedAt: string;
+//   //   userId: string;
+//   //   login: string;
+//   // }>,
 // }
-// export type SuperPostStaticType = {
-//   createSuperPost: () => string
-// }
-// export type PostDocument = HydratedDocument<Post>;
+
+export class Post {
+
+  _id: Types.ObjectId;
+
+  @Prop({
+    required: true
+  })
+  title: string;
+
+  @Prop({
+    required: true
+  })
+  shortDescription: string;
+
+  @Prop({
+    required: true
+  })
+  content: string;
+
+  @Prop({
+    required: true
+  })
+    // blogId: Types.ObjectId;
+  blogId: string;
+
+  @Prop({
+    required: true
+  })
+  blogName: string;
+
+  @Prop({
+    required: true
+  })
+  createdAt: string;
+
+  @Prop({
+    // type: ExtendedLikesInfoSchema
+  })
+    // extendedLikesInfo: ExtendedLikesInfo;
+  extendedLikesInfo: {
+    likesData: Array<{
+      _id: Types.ObjectId;
+      createdAt: string;
+      userId: Types.ObjectId;
+      userLogin: string;
+    }>;
+    dislikesData: Array<{
+      _id: Types.ObjectId;
+      createdAt: string;
+      userId: Types.ObjectId;
+      userLogin: string;
+    }>;
+    likesCount: number;
+    dislikesCount: number;
+    myStatus: "None" | "Like" | "Dislike";
+    newestLikes: Array<{
+      _id: Types.ObjectId;
+      createdAt: string;
+      userId: Types.ObjectId;
+      userLogin: string;
+    }>;
+  };
+
+  updatePost() {
+  }
+
+  static createStaticPost(
+    dto: CreatePostInputModelType,
+    blog: BlogDocument,
+    PostModel: PostModelType
+  ): PostDocument {
+    if (!dto) throw new Error("Bad request");
+    const createNewPost = new PostModel();
+    createNewPost.title = dto.title;
+    createNewPost.shortDescription = dto.shortDescription;
+    createNewPost.content = dto.content;
+    createNewPost.blogId = dto.blogId;
+    createNewPost.blogName = blog.name;
+    createNewPost.createdAt = new Date().toISOString();
+    createNewPost.extendedLikesInfo = {
+      likesData: [],
+      dislikesData: [],
+      likesCount: 0,
+      dislikesCount: 0,
+      myStatus: "None",
+      newestLikes: []
+    };
+
+    // extendedLikesInfo: {
+    //   likesData: [{
+    //     _id: Types.ObjectId,
+    //     createdAt: string,
+    //     userId: Types.ObjectId,
+    //     userLogin: string
+    //   }],
+    //     dislikesData;
+    // :
+    //   [{
+    //     _id: Types.ObjectId,
+    //     createdAt: string,
+    //     userId: Types.ObjectId,
+    //     userLogin: string
+    //   }],
+    //     likesCount;
+    // :
+    //   number;
+    //   dislikesCount: number;
+    //   myStatus: "None" | "Like" | "Dislike";
+    //   newestLikes: {
+    //     description: string;
+    //     addedAt: string;
+    //     userId: string;
+    //     login: string;
+    //   }
+    // }
+
+    return createNewPost;
+  }
+
+}
+
+export const PostSchema = SchemaFactory.createForClass(Post);
+PostSchema.methods = {
+  updatePost: Post.prototype.updatePost
+};
+const postStaticMethods: PostModelStaticType = {
+  createStaticPost: Post.createStaticPost
+};
+PostSchema.statics = postStaticMethods;
+export type PostModelStaticType = {
+  createStaticPost: (dto: CreatePostInputModelType, blog: BlogDocument, PostModel: PostModelType) => PostDocument
+}
+export type PostDocument = HydratedDocument<Post>
+export type PostModelType = Model<PostDocument> & PostModelStaticType
