9dce50d88215e1ec6f3e9d585c922c7c691a5d56
Alex <alexandepp@gmail.com>
2023-05-16 03:27:08 +0300

    add public decorator

---
+++ src/auth/decorators/public.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext, SetMetadata } from "@nestjs/common";
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+export const CurrentUserId = createParamDecorator(
+  (data: unknown, context: ExecutionContext): number => {
+    const request = context.switchToHttp().getRequest();
+    if (!request.user?.id) throw new Error('JwtGuard must be used')
+    return request.user.id;
+  }
+)
+export const BASIC_AUTH_KEY = 'basicAuth';
+export const BasicAuth = () => SetMetadata(BASIC_AUTH_KEY, true);
