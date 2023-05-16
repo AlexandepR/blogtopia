7427802c0ec88eb4b446a0bd689c7a8b1e0a333d
Alex <alexandepp@gmail.com>
2023-05-16 03:24:23 +0300

    add basic auth guard

---
+++ src/auth/guards/basic.auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { BasicStrategy } from "passport-http";
+import { settingsEnv } from "../../settings/settings";
+
+@Injectable()
+export class BasicAuthGuard extends PassportStrategy(BasicStrategy, 'basic') {
+  constructor(
+  //   private readonly authService: AuthService,
+  // private readonly configService: ConfigService,
+  ) {
+    super({
+      // usernameField: 'username',
+      // passwordField: 'password',
+    });
+  }
+
+  async validate(username: string, password: string): Promise<any> {
+    if (
+      settingsEnv.BASIC_LOGIN === username &&
+      settingsEnv.BASIC_PASS === password
+    ) {
+      return true;
+    }
+    throw new UnauthorizedException();
+  }
+}
