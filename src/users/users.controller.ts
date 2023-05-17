3217822f3ef295b9e9ab41d0725495804a913d67
Alex <alexandepp@gmail.com>
2023-05-17 16:57:04 +0300

    check users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,39 +1,67 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Query } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus, Ip,
+  Param,
+  Post,
+  Query, Req,
+  ValidationPipe
+} from "@nestjs/common";
 import { UsersService } from "./users.service";
-import { CreateUserInputModelType, ParamsUsersType } from "./type/usersTypes";
+import { CreateUserInputClassModel, ParamsUsersType } from "./type/usersTypes";
+import { BasicAuth } from "../auth/decorators/public.decorator";
+import { checkObjectId } from "../helpers/validation.helpers";
 
 
 @Controller("users")
 export class UsersController {
   constructor(protected usersService: UsersService) {
   }
+  @BasicAuth()
   @Get()
   async getUsers(
     @Query() query: ParamsUsersType
   ) {
     return this.usersService.findAll(query);
   }
-  @Get(":id")
-  async getUser(
-    @Param("id")
-      id: string
-  ) {
-    return this.usersService.getUser(id);
-  }
+  // @UseGuards(BasicAuthGuard)
+  // @Get(":id")
+  // async getUser(
+  //   @Param("id")
+  //     id: string
+  // ) {
+  //   return this.usersService.getUser(id);
+  // }
+  @BasicAuth()
   @Post()
-  async createUser(@Body() dto: CreateUserInputModelType) {
-    return await this.usersService.createUser(dto);
+  @Get(":id")
+  async getUserById(
+    @Param(ValidationPipe)
+    params: checkObjectId
+  ) {
+    return await this.usersService.findUserById(params.id)
   }
-
+  async createUser(
+    @Ip() ip,
+    @Body() dto: CreateUserInputClassModel
+  ) {
+    // dto.email='';
+    // const ip = ''
+    return await this.usersService.createUser(dto,ip);
+  }
+  @BasicAuth()
   @Delete(":id")
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteUser(
-    @Param("id")
-      id: string) {
-    await this.usersService.deleteUser(id);
-    return `This user #${id} removes`;
+    @Param(ValidationPipe)
+      params: checkObjectId) {
+    await this.usersService.deleteUser(params.id);
+    return `This user #${params.id} removes`;
   }
-
+  @BasicAuth()
   @Delete()
   async deleteAllUser() {
     await this.usersService.deleteAllUser();
