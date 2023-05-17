4a5791844e6ed5bbf5359529ab28eee26319a0f6
Alex <alexandepp@gmail.com>
2023-05-17 16:54:54 +0300

    fix users schema

--- src/users/type/users.schema.ts
+++ src/users/type/users.schema.ts
@@ -1,44 +1,142 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { HydratedDocument, Model, Types } from "mongoose";
 import { CreateUserInputModelType } from "./usersTypes";
+import { v4 as uuidv4 } from "uuid";
+import { add } from "date-fns";
+
+
+// @Schema({
+//   // _id: false,
+//   // versionKey: false
+// })
+// export class DateIp {
+//   @Prop()
+//   dateIp: Date [] | string[];
+// }
+
+// @Schema({
+//   _id: false,
+//   versionKey: false
+// })
+// export class SendDate {
+//   @Prop()
+//   sendDate: Date[] | string []
+// }
+@Schema()
+class UserAccount {
+  @Prop()
+  login: string;
+  @Prop()
+  email: string;
+  @Prop()
+  passwordHash: string;
+  @Prop()
+  createdAt:string;
+}
+@Schema({
+  _id: false,
+  versionKey: false
+})
+class EmailConfirmation {
+  @Prop()
+  confirmationCode: string;
+  @Prop()
+  expirationDate: Date;
+  @Prop()
+  isConfirmed: boolean;
+  @Prop()
+  passwordRecoveryCode: string;
+  // @Prop({type: SendDate})
+  @Prop()
+  sendEmails: Date[]
+}
+@Schema({
+  _id: false,
+  versionKey: false
+})
+class RegistrationData {
+  @Prop()
+  ip: string[]
+  // @Prop({type: DateIp})
+  @Prop()
+  infoDateIp: Date[]
+}
+@Schema({
+  _id: false,
+  versionKey: false
+})
+class AuthData {
+  @Prop()
+  expirationRefreshToken: string[]
+}
+@Schema({
+  _id: false,
+  versionKey: false
+})
+class AuthDevicesSessions {
+  @Prop()
+  ip: string[]
+  @Prop()
+  deviceId: string[]
+  @Prop()
+  title: string[]
+  @Prop()
+  lastActiveDate: string[]
+  @Prop()
+  expirationTokenDate: string[]
+}
 
 @Schema({
-  versionKey: false,
+  versionKey: false
 })
 export class User {
   _id: Types.ObjectId;
+  @Prop({ type: UserAccount })
+  accountData: UserAccount;
+  @Prop({ type: EmailConfirmation })
+  emailConfirmation: EmailConfirmation;
+  @Prop({ type: RegistrationData })
+  registrationData: RegistrationData;
+  @Prop({ type: AuthData })
+  authData: AuthData;
+  @Prop({ type: AuthDevicesSessions })
+  authDevicesSessions: AuthDevicesSessions;
 
-  @Prop({
-    required: true,
-    minlength: 3,
-    maxlength: 10,
-    match: /^[a-zA-Z0-9_-]*$/
-  })
-  login: string;
+  static create(
+    userDto: CreateUserInputModelType,
+    UserModel: UserModelType,
+    passwordHash: string,
+    ip: any
+  ): UserDocument {
+    const user = new UserModel();
+    user.accountData = new UserAccount();
+    user.emailConfirmation = new EmailConfirmation()
+    user.registrationData = new RegistrationData()
+    user.authData = new AuthData()
+    user.authDevicesSessions = new AuthDevicesSessions()
 
-  @Prop({
-    required: true,
-    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-  })
-  email: string;
+    user.accountData.login = userDto.login
+    user.accountData.email = userDto.email;
+    user.accountData.passwordHash = passwordHash;
+    user.accountData.createdAt = new Date().toISOString();
+    user.emailConfirmation.confirmationCode = uuidv4();
+    user.emailConfirmation.expirationDate = add(new Date(), {
+      hours: 1,
+      minutes: 3
+    });
+    user.emailConfirmation.isConfirmed = true;
+    user.emailConfirmation.passwordRecoveryCode = '';
+    user.registrationData.ip = [ip]
+    user.registrationData.infoDateIp = [];
+    user.authData.expirationRefreshToken = [];
+    user.authDevicesSessions.ip = []
+    user.authDevicesSessions.deviceId = []
+    user.authDevicesSessions.title = []
+    user.authDevicesSessions.lastActiveDate = []
+    user.authDevicesSessions.expirationTokenDate = []
 
-  @Prop({
-    required: true,
-    minlength: 6,
-    maxlength: 20
-  })
-  password: string;
 
-  @Prop()
-  createdAt: string;
-
-  static create(userDto: CreateUserInputModelType, UserModel: UserModelType):UserDocument {
-    const createUser = new UserModel();
-    createUser.login = userDto.login;
-    createUser.email = userDto.email;
-    createUser.password = userDto.password;
-    createUser.createdAt = new Date().toISOString();
-    return createUser;
+    return user;
   }
 }
 
@@ -48,7 +146,12 @@ const userStaticMethods: UserModelStaticType = {
 };
 UserSchema.statics = userStaticMethods
 export type UserModelStaticType = {
-  create:(userDto: CreateUserInputModelType, UserModel: UserModelType) => UserDocument
+  create: (
+    userDto: CreateUserInputModelType,
+    UserModel: UserModelType,
+    passwordHash: string,
+    ip: any
+  ) => UserDocument
 }
 export type UserDocument = HydratedDocument<User>
 export type UserModelType = Model<UserDocument> & UserModelStaticType
