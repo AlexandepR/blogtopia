2b50bbf5755a51d6fa2872a29445a63e83774493
Alex <alexandepp@gmail.com>
2023-05-30 19:56:06 +0300

    add updateBlog-blogs-use-case

---
+++ src/blogs/use-cases/authUser/updateBlog-blogs-public-use-case.ts
@@ -0,0 +1,31 @@
+import { BlogDocument } from "../../type/blogs.schema";
+import { BlogsRepository } from "../../blogs.repository";
+import { HttpException, HttpStatus } from "@nestjs/common";
+import { Types } from "mongoose";
+import { BlogInputClassModel } from "../../type/blogsType";
+import { validateOrRejectModel } from "../../../utils/validation.helpers";
+import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
+
+
+export class updateBlogCommand {
+  constructor(
+    public blogId: string,
+    public UpdateBlogDto: BlogInputClassModel,
+  ) {
+  }
+}
+
+@CommandHandler(updateBlogCommand)
+export class updateBlog implements ICommandHandler<updateBlogCommand>{
+  constructor(protected blogsRepository: BlogsRepository,
+  ) {
+  }
+  async execute(command: updateBlogCommand): Promise<BlogDocument> {
+    await validateOrRejectModel(command.UpdateBlogDto, BlogInputClassModel);
+    const blogId = new Types.ObjectId(command.blogId);
+    const blog = await this.blogsRepository.findBlogById(blogId);
+    if (!blog) throw new HttpException("", HttpStatus.NOT_FOUND);
+    blog.updateBlog(command.UpdateBlogDto);
+    return await this.blogsRepository.save(blog);
+  }
+}
