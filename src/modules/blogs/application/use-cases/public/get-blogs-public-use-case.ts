bb2be63811aa6d6e8f6076e7a8ee4800284b11b2
Alex <alexandepp@gmail.com>
2023-06-14 02:17:07 +0300

    change st get-blogs-p

--- src/modules/blogs/application/use-cases/public/get-blogs-public-use-case.ts
+++ src/modules/blogs/application/use-cases/public/get-blogs-public-use-case.ts
@@ -4,37 +4,37 @@ import { BlogsRepository } from "../../blogs.repository";
 import { pagesCounter, parseQueryPaginator, skipPage } from "../../../../../utils/helpers";
 import { BlogDocument } from "../../../type/blogs.schema";
 import { UsersRepository } from "../../../../users/application/users.repository";
+import { GetBlogsPublicFilter } from "../../../../../utils/filters/blog.filters";
 
 export class GetBlogsCommand {
   constructor(
-    public query: ParamsTypeClassModel,
-  ) {}
+    public query: ParamsTypeClassModel
+  ) {
+  }
 }
 
 @CommandHandler(GetBlogsCommand)
-export class GetBlogsPublicUseCase implements ICommandHandler<GetBlogsCommand>{
+export class GetBlogsPublicUseCase implements ICommandHandler<GetBlogsCommand> {
   constructor(
     protected blogsRepository: BlogsRepository,
-    protected usersRepository: UsersRepository,
-    ) {
+    protected usersRepository: UsersRepository
+  ) {
   }
   async execute(command: GetBlogsCommand): Promise<PaginationType<BlogDocument[]>> {
-      const { searchNameTerm, pageSize, pageNumber, sortDirection, sortBy } = parseQueryPaginator(command.query);
+    const { searchNameTerm, pageSize, pageNumber, sortDirection, sortBy } = parseQueryPaginator(command.query);
     const banUsers: Array<string> = await this.usersRepository.getBannedUsers();
-    const filter = ({ $or: [
-        { "blogOwnerInfo.userLogin": { $nin: banUsers } },
-        { name: { $regex: `${searchNameTerm}`, $options: "i" } }]
-    });
-      const getTotalCountBlogs = await this.blogsRepository.getTotalCountBlogs(filter);
-      const skip = skipPage(pageNumber, pageSize);
-      const pagesCount = pagesCounter(getTotalCountBlogs, pageSize);
-      const getBlogs = await this.blogsRepository.getBlogs(skip, pageSize, filter, sortBy, sortDirection);
-      return {
-        pagesCount: pagesCount,
-        page: pageNumber,
-        pageSize: pageSize,
-        totalCount: getTotalCountBlogs,
-        items: getBlogs
-      };
-    }
+    const banBlogs = await this.blogsRepository.getArrayIdBanBlogs();
+    const filter = GetBlogsPublicFilter(banUsers, banBlogs, searchNameTerm);
+    const getTotalCountBlogs = await this.blogsRepository.getTotalCountBlogs(filter);
+    const skip = skipPage(pageNumber, pageSize);
+    const pagesCount = pagesCounter(getTotalCountBlogs, pageSize);
+    const getBlogs = await this.blogsRepository.getBlogs(skip, pageSize, filter, sortBy, sortDirection);
+    return {
+      pagesCount: pagesCount,
+      page: pageNumber,
+      pageSize: pageSize,
+      totalCount: getTotalCountBlogs,
+      items: getBlogs
+    };
+  }
 }
