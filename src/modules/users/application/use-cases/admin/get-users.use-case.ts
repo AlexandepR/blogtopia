3b02b5318fe645c6fd5090eae6e049ed082ff101
Alex <alexandepp@gmail.com>
2023-06-26 13:40:35 +0300

    change admin-get-users sql

--- src/modules/users/application/use-cases/admin/get-users.use-case.ts
+++ src/modules/users/application/use-cases/admin/get-users.use-case.ts
@@ -1,65 +1,36 @@
-import { PaginationType } from "../../../../../types/types";
-import { BlogDocument } from "../../../../blogs/domain/entities/blogs.schema";
-import { pagesCounter, parseQueryUsersPaginator, skipPage } from "../../../../../utils/helpers";
-import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
-import { UsersRepository } from "../../../infrastructure/users.repository";
-import { GetUsersOutputModelType, ParamsUsersType } from "../../../type/usersTypes";
-import { UserDocument } from "../../../domain/entities/users.schema";
+import { PaginationType } from '../../../../../types/types';
+import { filterGetUsers, pagesCounter, parseQueryUsersPaginator, skipPage } from '../../../../../utils/helpers';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { GetUsersOutputModelType, ParamsUsersType } from '../../../type/usersTypes';
+import { UsersSqlRepository } from '../../../infrastructure/users.sql-repository';
 
 
 export class GetUsersByAdminCommand {
-  constructor(
-    public query: ParamsUsersType
-  ) {
-  }
+    constructor(
+        public query: ParamsUsersType
+    ) {
+    }
 }
 
 @CommandHandler(GetUsersByAdminCommand)
 export class GetUsersByAdminUseCase implements ICommandHandler<GetUsersByAdminCommand> {
-  constructor(
-    protected usersRepository: UsersRepository
-  ) {
-  }
-  async execute(command: GetUsersByAdminCommand): Promise<PaginationType<GetUsersOutputModelType[]>> {
-    const { filter, pageSize, pageNumber, sortDirection, sortBy, banStatus } = parseQueryUsersPaginator(command.query);
-    // const banUsers: Array<string> = await this.usersRepository.getBannedUsers();
-    // const filterWithBan = ({ "accountData.banInfo.isBanned": { $nin: true }, ...filter });
-    const totalCountUsers = await this.usersRepository.getTotalCountUsers(filter,banStatus);
-    const skip = skipPage(pageNumber, pageSize);
-    const pagesCount = pagesCounter(totalCountUsers, pageSize);
-    // const getTotalCount = await this.usersRepository.getTotalCountUsers(filter)
-    const allUsers = await this.usersRepository.getUsers(skip, pageSize, filter, sortBy, sortDirection,banStatus);
-    return allUsers
-    // if (allUsers) {
-    //   const users = allUsers.map(({
-    //                                 _id,
-    //                                 accountData: { login, email, passwordHash, createdAt, banInfo },
-    //                                 emailConfirmation
-    //                               }) => ({
-    //       // let userStatus: 'None' | 'Like' | 'Dislike' = 'None';
-    //       // if (userId) {
-    //       //   const userLike = extendedLikesInfo.likesData.find((like) => like.userId.toString() === userId.toString());
-    //       //   const userDislike = extendedLikesInfo.dislikesData.find((dislike) => dislike.userId.toString() === userId.toString());
-    //       //   userStatus = userLike ? 'Like' : userDislike ? 'Dislike' : 'None';
-    //       // }
-    //       // return {
-    //       //
-    //       // };
-    //       id: _id.toString(),
-    //       login: login,
-    //       email: email,
-    //       createdAt: createdAt,
-    //       banInfo,
-    //     })
-    //   );
-    //   return {
-    //     pagesCount: pagesCount,
-    //     page: pageNumber,
-    //     pageSize: pageSize,
-    //     totalCount: totalCountUsers,
-    //     items: users
-    //   };
-    // }
-    // return null;
-  }
+    constructor(
+        protected usersSqlRepository: UsersSqlRepository
+    ) {
+    }
+    async execute(command: GetUsersByAdminCommand): Promise<PaginationType<GetUsersOutputModelType[] | null>> {
+        const filter = filterGetUsers(command.query);
+        const { pageSize, pageNumber, sortDirection, sortBy, banStatus } = parseQueryUsersPaginator(command.query);
+        const totalCountUsers = await this.usersSqlRepository.getTotalCountUsers(filter, banStatus);
+        const skip = skipPage(pageNumber, pageSize);
+        const pagesCount = pagesCounter(totalCountUsers, pageSize);
+        const allUsers = await this.usersSqlRepository.getUsers(skip, pageSize, filter, sortBy, sortDirection, banStatus);
+        return {
+            pagesCount: pagesCount,
+            page: pageNumber,
+            pageSize: pageSize,
+            totalCount: totalCountUsers,
+            items: allUsers
+        };
+    }
 }
