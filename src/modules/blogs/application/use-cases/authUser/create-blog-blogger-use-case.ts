52bd3699f9321501480bd8e63053a0d74641c920
Alex <alexandepp@gmail.com>
2023-06-30 19:15:55 +0300

    change case for create blog and post

--- src/modules/blogs/application/use-cases/authUser/create-blog-blogger-use-case.ts
+++ src/modules/blogs/application/use-cases/authUser/create-blog-blogger-use-case.ts
@@ -10,30 +10,25 @@ import {
 import { BlogInputClassModel, BlogType } from "../../../type/blogsType";
 import { validateOrRejectModel } from "../../../../../utils/validation.helpers";
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
+import { BlogsSqlRepository } from '../../../infrastructure/blogs.sql-repository';
+import { FindUserType } from '../../../../users/type/usersTypes';
 
 export class CreateBlogCommand {
   constructor(
-    public user: UserDocument,
+    public user: FindUserType,
     public dto: BlogInputClassModel
   ) {}
 }
 
 @CommandHandler(CreateBlogCommand)
 export class CreateBlogByBloggerUseCase implements ICommandHandler<CreateBlogCommand>{
-  constructor(protected blogsRepository: BlogsRepository) {
+  constructor(protected blogsSqlRepository: BlogsSqlRepository) {
   }
   async execute(command: CreateBlogCommand): Promise<BlogType> {
     await validateOrRejectModel(command.dto, BlogInputClassModel);
     if(!command.user) {throw new BadRequestException()}
-    const createBlog = await this.blogsRepository.createBlog(command.dto,command.user);
+    const createBlog = await this.blogsSqlRepository.createBlog(command.dto,command.user);
     if (!createBlog) throw new HttpException("", HttpStatus.NOT_FOUND);
-    return {
-      id: createBlog._id.toHexString(),
-      name: createBlog.name,
-      description: createBlog.description,
-      websiteUrl: createBlog.websiteUrl,
-      createdAt: createBlog.createdAt,
-      isMembership: createBlog.isMembership
-    };
+    return createBlog
   }
 }
--- src/modules/blogs/application/use-cases/authUser/createPostForBlog-blogger-use-case.ts
+++ src/modules/blogs/application/use-cases/authUser/createPostForBlog-blogger-use-case.ts
@@ -1,18 +1,22 @@
 import { UserDocument } from "../../../../users/domain/entities/users.schema";
 import { BlogsRepository } from "../../../infrastructure/blogs.repository";
-import { ForbiddenException, NotFoundException } from "@nestjs/common";
+import { ForbiddenException, HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
 import { PostForBlogBloggerInputClassModel } from "../../../type/blogsType";
-import { outputPostModelType } from "../../../../posts/type/postsType";
+import { outputPostModelType, PostType } from '../../../../posts/type/postsType';
 import { validateOrRejectModel } from "../../../../../utils/validation.helpers";
 import { Types } from "mongoose";
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { PostsRepository } from "../../../../posts/infrastructure/posts.repository";
 import { BlogsQueryRepository } from "../../../infrastructure/blogs.query-repository";
+import { BlogsQuerySqlRepository } from '../../../infrastructure/blogs.sql.query-repository';
+import { validateIdByUUID } from '../../../../../utils/helpers';
+import { FindUserType } from '../../../../users/type/usersTypes';
+import { PostsSqlRepository } from '../../../../posts/infrastructure/posts.sql-repository';
 
 
 export class CreatePostByBlogCommand {
   constructor(
-    public user: UserDocument,
+    public user: FindUserType,
     public dto: PostForBlogBloggerInputClassModel,
     public blogId: string
   ) {
@@ -22,41 +26,41 @@ export class CreatePostByBlogCommand {
 @CommandHandler(CreatePostByBlogCommand)
 export class CreatePostByBlogByBloggerUseCase implements ICommandHandler<CreatePostByBlogCommand> {
   constructor(
-    protected blogsRepository: BlogsRepository,
-    protected blogsQueryRepository: BlogsQueryRepository,
-    protected postsRepository: PostsRepository
+    protected blogsQuerySqlRepository: BlogsQuerySqlRepository,
+    protected postsSqlRepository: PostsSqlRepository
   ) {
   }
   async execute(command: CreatePostByBlogCommand)
-    : Promise<outputPostModelType | null> {
+    : Promise<outputPostModelType> {
     await validateOrRejectModel(command.dto, PostForBlogBloggerInputClassModel);
-    if (!Types.ObjectId.isValid(command.blogId)) {
-      throw new NotFoundException();
-    }
-    const blogId = new Types.ObjectId(command.blogId);
-    const getBlog = await this.blogsQueryRepository.findBlogById(blogId);
+    const blogId = validateIdByUUID(command.blogId)
+    if(!blogId) throw new HttpException("", HttpStatus.NOT_FOUND);
+    const getBlog = await this.blogsQuerySqlRepository.findBlogById(blogId);
     if (!getBlog) throw new NotFoundException();
-    if (getBlog.blogOwnerInfo.userLogin !== command.user.accountData.login ||
-      getBlog.blogOwnerInfo.userId !== command.user._id.toString()) throw new ForbiddenException();
-    // if(getBlog.blogOwnerInfo.userLogin !== command.user.accountData.login) throw new ForbiddenException()
+    if (getBlog.BlogOwnerLogin !== command.user.login ||
+      getBlog.BlogOwnerId !== command.user.ID) throw new ForbiddenException();
     const dto = {
       ...command.dto,
       blogId: command.blogId
     };
-    const createPost = await this.postsRepository.createPost(dto, getBlog, command.user);
+    const post = await this.postsSqlRepository.createPost(dto, getBlog, command.user);
     return {
-      id: createPost._id.toString(),
-      title: createPost.title,
-      shortDescription: createPost.shortDescription,
-      content: createPost.content,
-      blogId: createPost.blogId,
-      blogName: createPost.blogName,
-      createdAt: createPost.createdAt,
+      id: post.ID,
+      title: post.title,
+      shortDescription: post.shortDescription,
+      content: post.content,
+      blogId: post.blogId,
+      blogName: post.blogName,
+      createdAt: post.createdAt,
       extendedLikesInfo: {
-        likesCount: createPost.extendedLikesInfo.likesCount,
-        dislikesCount: createPost.extendedLikesInfo.dislikesCount,
-        myStatus: createPost.extendedLikesInfo.myStatus,
-        newestLikes: createPost.extendedLikesInfo.newestLikes
+        likesCount: post.likesCount,
+        dislikesCount: post.dislikesCount,
+        myStatus: post.myStatus,
+        newestLikes: [{
+          addedAt: "2023-06-30T11:40:50.588Z",
+          userId: "string",
+          login: "string"
+        }]
       }
     };
   }
