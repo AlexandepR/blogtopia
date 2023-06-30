0b382a15a167eaeff584478144289fee9c9e7b13
Alex <alexandepp@gmail.com>
2023-06-30 22:21:08 +0300

    change updateBlog-Blogger-sql

--- src/modules/blogs/application/use-cases/authUser/updateBlog-blogger-use-case.ts
+++ src/modules/blogs/application/use-cases/authUser/updateBlog-blogger-use-case.ts
@@ -1,4 +1,4 @@
-import { BlogDocument } from "../../../domain/entities/blogs.schema";
+import { BlogDocument } from "../../../domain/blogs.schema";
 import { BlogsRepository } from "../../../infrastructure/blogs.repository";
 import { ForbiddenException, HttpException, HttpStatus, NotFoundException } from "@nestjs/common";
 import { Types } from "mongoose";
@@ -7,13 +7,17 @@ import { validateOrRejectModel } from "../../../../../utils/validation.helpers";
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { UserDocument } from "../../../../users/domain/entities/users.schema";
 import { BlogsQueryRepository } from "../../../infrastructure/blogs.query-repository";
+import { FindUserType } from '../../../../users/type/usersTypes';
+import { validateIdByUUID } from '../../../../../utils/helpers';
+import { BlogsSqlRepository } from '../../../infrastructure/blogs.sql-repository';
+import { BlogsQuerySqlRepository } from '../../../infrastructure/blogs.sql.query-repository';
 
 
 export class UpdateBlogCommand {
   constructor(
     public blogId: string,
-    public UpdateBlogDto: BlogInputClassModel,
-    public user: UserDocument,
+    public updateBlogDto: BlogInputClassModel,
+    public user: FindUserType,
   ) {
   }
 }
@@ -21,19 +25,16 @@ export class UpdateBlogCommand {
 @CommandHandler(UpdateBlogCommand)
 export class UpdateBlogByBloggerUseCase implements ICommandHandler<UpdateBlogCommand>{
   constructor(
-    protected blogsRepository: BlogsRepository,
-    protected blogsQueryRepository: BlogsQueryRepository,
+    protected blogsSqlRepository: BlogsSqlRepository,
+    protected blogsQuerySqlRepository: BlogsQuerySqlRepository,
   ) {
   }
-  async execute(command: UpdateBlogCommand): Promise<BlogDocument> {
-    await validateOrRejectModel(command.UpdateBlogDto, BlogInputClassModel);
-    if(!Types.ObjectId.isValid(command.blogId)) {throw new NotFoundException()}
-    const blogId = new Types.ObjectId(command.blogId);
-    // const filterIsOwn = ({'blogOwnerInfo.userLogin': command.user.accountData.login})
-    const blog = await this.blogsQueryRepository.findBlogById(blogId);
+  async execute(command: UpdateBlogCommand): Promise<boolean> {
+    await validateOrRejectModel(command.updateBlogDto, BlogInputClassModel);
+    if(!validateIdByUUID(command.blogId)) {throw new NotFoundException()}
+    const blog = await this.blogsQuerySqlRepository.findBlogById(command.blogId);
     if (!blog) throw new HttpException("", HttpStatus.NOT_FOUND);
-    if(blog.blogOwnerInfo.userLogin !== command.user.accountData.login) throw new ForbiddenException()
-    blog.updateBlog(command.UpdateBlogDto);
-    return await blog.save();
+    if(blog.BlogOwnerLogin !== command.user.login) throw new ForbiddenException()
+    return await this.blogsSqlRepository.updateBlog(command.updateBlogDto, command.blogId)
   }
 }
