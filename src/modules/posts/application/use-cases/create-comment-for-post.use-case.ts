96294e007986b543752d4794d943d1c4f4849a23
Alex <alexandepp@gmail.com>
2023-07-10 14:04:15 +0300

    refactor create-comment

--- src/modules/posts/application/use-cases/create-comment-for-post.use-case.ts
+++ src/modules/posts/application/use-cases/create-comment-for-post.use-case.ts
@@ -1,60 +1,53 @@
-import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
-import { validateOrRejectModel } from "../../../../utils/validation.helpers";
-import { validateObjectId } from "../../../../utils/helpers";
-import { ForbiddenException, HttpException, HttpStatus } from "@nestjs/common";
-import { UserDocument } from "../../../users/domain/entities/users.schema";
-import { getCommentsByPostOutputModel } from "../../type/postsType";
-import { UsersRepository } from "../../../users/infrastructure/users.repository";
-import { PostsRepository } from "../../infrastructure/posts.repository";
-import { CreateCommentInputClassModel } from "../posts.service";
-import { BlogsRepository } from "../../../blogs/infrastructure/blogs.repository";
-import { BlogsQueryRepository } from "../../../blogs/infrastructure/blogs.query-repository";
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { validateOrRejectModel } from '../../../../utils/validation.helpers';
+import { validateIdByUUID } from '../../../../utils/helpers';
+import { HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
+import { CommentByPostType } from '../../type/postsType';
+import { CreateCommentInputClassModel } from '../posts.service';
+import { FindUserType } from '../../../users/type/usersTypes';
+import { PostsSqlRepository } from '../../infrastructure/posts.sql-repository';
 
 
 export class CreateCommentForPostCommand {
-  constructor(
-    public postId: string,
-    public dto: CreateCommentInputClassModel,
-    public user: UserDocument,
-  ) {
-  }
+    constructor(
+        public postId: string,
+        public dto: CreateCommentInputClassModel,
+        public user: FindUserType,
+    ) {
+    }
 }
 
 @CommandHandler(CreateCommentForPostCommand)
 export class CreateCommentForPostUseCase implements ICommandHandler<CreateCommentForPostCommand> {
-  constructor(
-    protected usersRepository: UsersRepository,
-    protected postsRepository: PostsRepository,
-    protected blogsRepository: BlogsRepository,
-    protected blogsQueryRepository: BlogsQueryRepository,
-  ) {
-  }
-  async execute(command: CreateCommentForPostCommand): Promise<getCommentsByPostOutputModel> {
-    await validateOrRejectModel(command.dto, CreateCommentInputClassModel);
-    const postId = validateObjectId(command.postId);
-    if(!postId) throw new HttpException('', HttpStatus.NOT_FOUND)
-    const post = await this.postsRepository.findPostById(postId)
-    if(!post) throw new HttpException('', HttpStatus.NOT_FOUND)
-    const blog = await this.blogsQueryRepository.findBlogById(post.blogId)
-    if(blog.banUsersInfo.find((ban) => ban.login === command.user.accountData.login)) {throw new ForbiddenException()}
-    const createComment = await this.postsRepository.createComment(command.dto.content, post, command.user);
-    if(!createComment) throw new HttpException('', HttpStatus.NOT_FOUND)
-    if (createComment) {
-      return {
-        id: createComment._id.toString(),
-        content: createComment.content,
-        commentatorInfo: {
-          userId: createComment.commentatorInfo.userId.toString(),
-          userLogin: createComment.commentatorInfo.userLogin
-        },
-        createdAt: createComment.createdAt,
-        likesInfo: {
-          likesCount: createComment.likesInfo.likesCount,
-          dislikesCount: createComment.likesInfo.dislikesCount,
-          myStatus: createComment.likesInfo.myStatus,
-        }
-      };
+    constructor(
+        protected postsSqlRepository: PostsSqlRepository,
+    ) {
     }
-    else {throw new HttpException('', HttpStatus.BAD_REQUEST)}
-  }
+    async execute({ dto, postId, user }: CreateCommentForPostCommand): Promise<CommentByPostType> {
+        await validateOrRejectModel(dto, CreateCommentInputClassModel);
+        if (!validateIdByUUID(postId)) throw new NotFoundException();
+        const post = await this.postsSqlRepository.findPostById(postId);
+        if (!post) throw new HttpException('', HttpStatus.NOT_FOUND);
+        const comment = await this.postsSqlRepository.createComment(dto.content, postId, user);
+        if (!comment) throw new HttpException('', HttpStatus.NOT_FOUND);
+        console.log(comment);
+        if (comment) {
+            return {
+                id: comment.ID,
+                content: comment.content,
+                commentatorInfo: {
+                    userId: comment.commentatorId,
+                    userLogin: comment.commentatorLogin
+                },
+                createdAt: comment.createdAt,
+                likesInfo: {
+                    likesCount: comment.likesCount,
+                    dislikesCount: comment.dislikesCount,
+                    myStatus: comment.myStatus,
+                }
+            };
+        } else {
+            throw new HttpException('', HttpStatus.BAD_REQUEST);
+        }
+    }
 }
