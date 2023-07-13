c1ea6c85dfde58d8b7ef16d9411503943bae0bf2
Alex <alexandepp@gmail.com>
2023-07-13 18:21:45 +0300

    refactor get-comment

--- src/modules/posts/infrastructure/posts.sql-repository.ts
+++ src/modules/posts/infrastructure/posts.sql-repository.ts
@@ -5,12 +5,9 @@ import {
     CreatePostForBlogInputClassModel,
     CreatePostInputModelType,
     likeStatusInputClassModel,
-    PostsNewestLikesType,
     PostType
 } from '../type/postsType';
-import { ObjectId } from 'mongodb';
 import { Comment, CommentModelType } from '../../comments/domain/comments.schema';
-import { Types } from 'mongoose';
 import { User, UserModelType } from '../../users/domain/entities/users.schema';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
@@ -35,9 +32,28 @@ export class PostsSqlRepository {
         blogId?: string,
         user?: FindUserType
     ): Promise<any> {
+        console.log(sortDirection,'sortDirection');
+        console.log(sortBy,'sortBy');
         let userId = '';
+        let whereCondition = '';
         if (user) {
             userId = user.ID;
+            whereCondition = `
+                CASE
+                    WHEN EXISTS (
+                        SELECT 1
+                        FROM public."PostLikesData" pld
+                        WHERE pld."postId" = p."ID"
+                        AND pld."userId" = '${userId}'
+                    ) THEN 'Like'
+                    WHEN EXISTS (
+                        SELECT 1
+                        FROM public."PostDislikesData" pdld
+                        WHERE pdld."postId" = p."ID"
+                        AND pdld."userId" = '${userId}'
+                    ) THEN 'Dislike'
+                      ELSE 'None'
+                 END`
         }
         const getQuery = `
         SELECT p."ID" as id, p."title", p."shortDescription", p."content",
@@ -55,26 +71,20 @@ export class PostsSqlRepository {
                     FROM public."PostDislikesData" pdld
                     WHERE pdld."postId" = p."ID"
                 ),
-                'myStatus',
-                 CASE
-                    WHEN EXISTS (
-                        SELECT 1
-                        FROM public."PostLikesData" pld
-                        WHERE pld."postId" = p."ID"
-                        AND pld."userId" = '${userId}'
-                    ) THEN 'Like'
-                    WHEN EXISTS (
-                        SELECT 1
-                        FROM public."PostDislikesData" pdld
-                        WHERE pdld."postId" = p."ID"
-                        AND pdld."userId" = '${userId}'
-                    ) THEN 'Dislike'
-                      ELSE 'None'
-                 END,
+                'myStatus', ${ whereCondition ? whereCondition : "'None'"},
                  'newestLikes', (
-                        SELECT json_agg(pld)
+                        SELECT json_agg(
+                        json_build_object(
+                            'addedAt', pld."addedAt",
+                            'userId', pld."userId",
+                            'login', pld."login"
+                            )
+                        )
                         FROM (
-                            SELECT *
+                            SELECT 
+                                pld."addedAt",
+                                pld."userId",
+                                pld."login"
                             FROM public."PostLikesData" pld
                             WHERE pld."postId" = p."ID"
                             ORDER BY pld."addedAt" DESC
@@ -82,20 +92,24 @@ export class PostsSqlRepository {
                     ) AS pld
                 )
             ) as "extendedLikesInfo"
-        FROM public."Posts" p, public."BanUserInfo" u, public."Blogs" b
-        WHERE p."blogId" =  ${ blogId ? `'${blogId}'` : "''" }
+        FROM public."Posts" p
+        JOIN public."Blogs" b ON p."blogId" = b."ID"
+        WHERE p."blogId" =  '${blogId}'
         AND NOT EXISTS (
                     SELECT 1
                     FROM public."BanUserInfo" ub
                     WHERE ub."isBanned" = true
                     AND ub."userId" = b."BlogOwnerId"
-                    AND p."blogId" = ${ blogId ? `'${blogId}'` : "''" }
+                    AND p."blogId" = '${blogId}'
                 )
-    ORDER BY ${sortBy === 'createdAt' ? 'b."createdAt"' : `"${sortBy}" COLLATE "C"`} ${sortDirection}
+    ORDER BY ${sortBy === 'createdAt' ? 'p."createdAt"' : `"${sortBy}" COLLATE "C"`} ${sortDirection}
     LIMIT ${pageSize}
     OFFSET ${skip}
     `;
         const result = await this.dataSource.query(getQuery);
+        result.map((el) => {if (!el.extendedLikesInfo.newestLikes) {
+            el.extendedLikesInfo.newestLikes = [];
+        }return el})
         return result;
     }
     async getPosts(
@@ -107,11 +121,9 @@ export class PostsSqlRepository {
         user?: FindUserType
     ): Promise<any> {
         let userId = '';
+        let whereCondition = ''
         if (user) {
             userId = user.ID;
-        }
-        let whereCondition = ''
-        if(user){
             whereCondition = `
                 CASE
                     WHEN EXISTS (
@@ -296,17 +308,6 @@ export class PostsSqlRepository {
             AND p."blogId" = b."ID" 
         )
        `;
-    //     'newestLikes', (
-    //         SELECT COALESCE(json_agg(pld), '[{"addedAt": "2000", "userId": "string", "login": "string"}]')
-    //     FROM (
-    //         SELECT *
-    //         FROM public."PostLikesData" pld
-    //     WHERE pld."postId" = p."ID"
-    //     ORDER BY pld."addedAt" DESC
-    //     LIMIT 3
-    // ) AS pld
-    // )
-    // ) as "extendedLikesInfo"
         const getPost = await this.dataSource.query(getQuery);
         if (!getPost[0].extendedLikesInfo.newestLikes ) getPost[0].extendedLikesInfo.newestLikes = [];
         if (getPost.length > 0) return getPost[0];
@@ -333,9 +334,9 @@ export class PostsSqlRepository {
         const getCountBlogsQuery = `
                  SELECT p.*
                  FROM public."Posts" p
-                 JOIN public."Blogs" b ON 
+                 JOIN public."Blogs" b ON b."ID" = p."blogId"
                  WHERE p."blogId" = '${blogId}'
-                     AND u."isBanned" != true
+                     AND b."isBanned" != true
                      AND NOT EXISTS (
                                      SELECT 1
                                      FROM public."BanUserInfo" ub
@@ -415,71 +416,4 @@ export class PostsSqlRepository {
         const rowCount = result ? result[1] : 0;
         return rowCount > 0;
     }
-    async deleteAllPosts(): Promise<boolean> {
-        const delPost = await this.PostModel
-            .deleteMany({});
-        return delPost.deletedCount >= 1;
-    }
-    async checkLikesInfo(postId: Types.ObjectId, userId: ObjectId, updateField: string): Promise<boolean> {
-        const field = `extendedLikesInfo.${updateField}`;
-        const result = await this.PostModel
-            .updateMany(
-                { _id: postId },
-                { $pull: { [field]: { userId: userId } } }
-            );
-        return result.modifiedCount > 0;
-    }
-    async checkNewestLikes(postId: ObjectId, userId: ObjectId): Promise<boolean> {
-        const checkNewestLikes = await this.PostModel
-            .updateMany(
-                { _id: postId },
-                { $pull: { 'extendedLikesInfo.newestLikes': { userId: userId } } }
-            );
-        return checkNewestLikes.modifiedCount > 0;
-    }
-    async updateNewestLikes(postId: ObjectId, newesetLike: PostsNewestLikesType): Promise<boolean> {
-        const updateNewestLike = await this.PostModel.updateOne(
-            { _id: postId },
-            {
-                $push: {
-                    'extendedLikesInfo.newestLikes': {
-                        $each: [newesetLike],
-                        $sort: { 'addedAt': -1 },
-                        $slice: 3
-                    }
-                }
-            }
-        );
-        return updateNewestLike.modifiedCount > 0;
-    }
-    async findPostFilterBanUser(postId: string): Promise<any> {
-        const getCountBlogsQuery = `
-                SELECT p.*
-                FROM public."Posts" p,
-                JOIN public."Blogs" b ON b."ID" = p."blogId"
-                JOIN public."BanUserInfo" u ON b."BlogOwnerId" = u."userId"
-                WHERE p."ID" = '${postId}'
-                AND u."isBanned" != true
-                AND NOT EXISTS (
-                    SELECT 1
-                    FROM public."BanUserInfo" ub
-                    WHERE ub."isBanned" = true
-                    AND ub."userId" = b."BlogOwnerId"
-                )
-    `;
-        const count = await this.dataSource.query(getCountBlogsQuery);
-    }
-    // async updatePostLikesInfo(post: PostDocument, postId: ObjectId): Promise<boolean> {
-    //     const updatePost = await this.PostModel
-    //         .updateOne({ _id: postId }, {
-    //             $set:
-    //                 {
-    //                     'extendedLikesInfo.likesData': post.extendedLikesInfo.likesData,
-    //                     'extendedLikesInfo.dislikesData': post.extendedLikesInfo.dislikesData,
-    //                     'extendedLikesInfo.likesCount': post!.extendedLikesInfo.likesData.length,
-    //                     'extendedLikesInfo.dislikesCount': post!.extendedLikesInfo.dislikesData.length
-    //                 }
-    //         });
-    //     return !!updatePost;
-    // }
 }
