cd4cfc8f6146b214a5bda299a82124d62d500e86
Alex <alexandepp@gmail.com>
2023-07-15 00:05:47 +0300

    check upd-comment

--- src/modules/posts/application/use-cases/update-post-like-status.use-case.ts
+++ src/modules/posts/application/use-cases/update-post-like-status.use-case.ts
@@ -1,93 +1,38 @@
-import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
-import { validateOrRejectModel } from "../../../../utils/validation.helpers";
-import { updatePostLikesInfo } from "../../../../utils/helpers";
-import { HttpException, HttpStatus, NotFoundException } from "@nestjs/common";
-import { Types } from "mongoose";
-import { UserDocument } from "../../../users/domain/entities/users.schema";
-import { likeStatusInputClassModel, PostLikesType, PostsNewestLikesType } from "../../type/postsType";
-import { UsersRepository } from "../../../users/infrastructure/users.repository";
-import { PostsRepository } from "../../infrastructure/posts.repository";
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { validateOrRejectModel } from '../../../../utils/validation.helpers';
+import { validateIdByUUID } from '../../../../utils/helpers';
+import { HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
+import { likeStatusInputClassModel } from '../../type/postsType';
+import { UsersSqlRepository } from '../../../users/infrastructure/users.sql-repository';
+import { PostsSqlRepository } from '../../infrastructure/posts.sql-repository';
+import { FindUserType } from '../../../users/type/usersTypes';
 
 
 export class UpdatePostLikeStatusCommand {
-  constructor(
-    public dto: likeStatusInputClassModel,
-    public id: string,
-    public user: UserDocument,
-  ) {
-  }
+    constructor(
+        public dto: likeStatusInputClassModel,
+        public postId: string,
+        public user: FindUserType,
+    ) {
+    }
 }
 
 @CommandHandler(UpdatePostLikeStatusCommand)
 export class UpdatePostLikeStatusUseCase implements ICommandHandler<UpdatePostLikeStatusCommand> {
-  constructor(
-    protected usersRepository: UsersRepository,
-    protected postsRepository: PostsRepository,
-  ) {
-  }
-  async execute(command: UpdatePostLikeStatusCommand): Promise<boolean> {
-    await validateOrRejectModel(command.dto, likeStatusInputClassModel);
-    const userId = command.user._id;
-    if(!Types.ObjectId.isValid(command.id)) {throw new NotFoundException()}
-    const postId = new Types.ObjectId(command.id)
-    const likeStatus = command.dto.likeStatus
-    const banUsers: Array<string> = await this.usersRepository.getBannedUsers();
-    const filter = (
-      { "postOwnerInfo.userLogin": { $nin: banUsers }
-      });
-    const post = await this.postsRepository.findPostByIdForBlogger(postId,filter);
-    if (!userId || !post) if (!post) throw new HttpException("", HttpStatus.NOT_FOUND);
-    const user =  await this.usersRepository.findUserById(userId)
-    const {accountData: { login: userLogin }} = user!
-
-    const newDate = new Date().toISOString();
-    const newLikesData: PostLikesType = {
-      _id: new Types.ObjectId(),
-      createdAt: newDate,
-      userId: userId,
-      userLogin: userLogin,
-    };
-    const newestLike: PostsNewestLikesType = {
-      description: '',
-      addedAt: newDate,
-      userId: new Types.ObjectId(userId),
-      login: userLogin
-    };
-    if (likeStatus === 'Like') {
-      const checkDislikes = await this.postsRepository.checkLikesInfo(postId, userId, 'dislikesData');
-      if (checkDislikes) {
-        const post = await this.postsRepository.findPostById(postId);
-        const updatePostLikesCount = updatePostLikesInfo(post!, likeStatus, newLikesData);
-        return await this.postsRepository.updatePostLikesInfo(updatePostLikesCount!, postId);
-      }
-      await this.postsRepository.checkLikesInfo(postId, userId, 'likesData');
-      await this.postsRepository.checkNewestLikes(postId, userId);
-      await this.postsRepository.updateNewestLikes(postId, newestLike);
-      const getPost = await this.postsRepository.findPostById(postId);
-      const updatePostLikesCount = updatePostLikesInfo(getPost!, likeStatus, newLikesData);
-      return await this.postsRepository.updatePostLikesInfo(updatePostLikesCount!, postId);
+    constructor(
+        protected usersSqlRepository: UsersSqlRepository,
+        protected postsSqlRepository: PostsSqlRepository,
+    ) {
     }
-    if (likeStatus === 'Dislike') {
-      const checkLikes = await this.postsRepository.checkLikesInfo(postId, userId, 'likesData');
-      if (checkLikes) {
-        await this.postsRepository.checkNewestLikes(postId, userId);
-        const post = await this.postsRepository.findPostById(postId);
-        const updatePostLikesCount = updatePostLikesInfo(post!, likeStatus, newLikesData);
-        return await this.postsRepository.updatePostLikesInfo(updatePostLikesCount!, postId);
-      }
-      await this.postsRepository.checkLikesInfo(postId, userId, 'dislikesData');
-      const getPost = await this.postsRepository.findPostById(postId);
-      const updatePostLikesCount = updatePostLikesInfo(getPost!, likeStatus, newLikesData);
-      return await this.postsRepository.updatePostLikesInfo(updatePostLikesCount!, postId);
+    async execute({ dto, postId, user: reqUser }: UpdatePostLikeStatusCommand): Promise<boolean> {
+        await validateOrRejectModel(dto, likeStatusInputClassModel);
+        if (!validateIdByUUID(postId)) throw new NotFoundException();
+        const post = await this.postsSqlRepository.findPostById(postId);
+        const user = await this.usersSqlRepository.findUserById(reqUser.ID);
+        if (!user || !post || user.banInfo.isBanned) throw new HttpException('', HttpStatus.NOT_FOUND);
+        if (dto.likeStatus === 'None') return await this.postsSqlRepository.deleteLikesStatus(user.ID, postId);
+        const updateLikesStatus = await this.postsSqlRepository.createPostLikesInfo(user.ID, user.login, postId, dto);
+        if (updateLikesStatus) throw new HttpException('', HttpStatus.NO_CONTENT);
+        throw new HttpException('', HttpStatus.NOT_FOUND);
     }
-    if (likeStatus === 'None') {
-      await this.postsRepository.checkLikesInfo(postId, userId, 'dislikesData');
-      await this.postsRepository.checkLikesInfo(postId, userId, 'likesData');
-      await this.postsRepository.checkNewestLikes(postId, userId);
-      const post = await this.postsRepository.findPostById(postId);
-      const updatePostLikesCount = updatePostLikesInfo(post!, likeStatus, newLikesData);
-      return await this.postsRepository.updatePostLikesInfo(updatePostLikesCount!, postId);
-    }
-    return false;
-  }
 }
