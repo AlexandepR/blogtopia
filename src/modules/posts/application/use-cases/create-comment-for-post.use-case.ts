3a2858d5d27f18738d7fc256862178e4da5d5a3e
Alex <alexandepp@gmail.com>
2023-07-15 00:04:45 +0300

    create comment add banUsers

--- src/modules/posts/application/use-cases/create-comment-for-post.use-case.ts
+++ src/modules/posts/application/use-cases/create-comment-for-post.use-case.ts
@@ -1,18 +1,20 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { validateOrRejectModel } from '../../../../utils/validation.helpers';
 import { validateIdByUUID } from '../../../../utils/helpers';
-import { HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
 import { CommentByPostType } from '../../type/postsType';
 import { CreateCommentInputClassModel } from '../posts.service';
 import { FindUserType } from '../../../users/type/usersTypes';
 import { PostsSqlRepository } from '../../infrastructure/posts.sql-repository';
+import { BlogsQuerySqlRepository } from '../../../blogs/infrastructure/blogs.sql.query-repository';
+import { CommentsSqlRepository } from '../../../comments/infrastructure/comments.sql-repository';
 
 
 export class CreateCommentForPostCommand {
     constructor(
         public postId: string,
-        public dto: CreateCommentInputClassModel,
         public user: FindUserType,
+        public dto: CreateCommentInputClassModel,
     ) {
     }
 }
@@ -21,6 +23,8 @@ export class CreateCommentForPostCommand {
 export class CreateCommentForPostUseCase implements ICommandHandler<CreateCommentForPostCommand> {
     constructor(
         protected postsSqlRepository: PostsSqlRepository,
+        protected blogsQuerySqlRepository: BlogsQuerySqlRepository,
+        protected commentsSqlRepository: CommentsSqlRepository,
     ) {
     }
     async execute({ dto, postId, user }: CreateCommentForPostCommand): Promise<CommentByPostType> {
@@ -28,9 +32,10 @@ export class CreateCommentForPostUseCase implements ICommandHandler<CreateCommen
         if (!validateIdByUUID(postId)) throw new NotFoundException();
         const post = await this.postsSqlRepository.findPostById(postId);
         if (!post) throw new HttpException('', HttpStatus.NOT_FOUND);
+        const getBannedUser = await this.blogsQuerySqlRepository.getBanUsersByBlogs(post.blogId)
+        if(getBannedUser.find((el) => el.userId === user.ID)) throw new ForbiddenException()
         const comment = await this.postsSqlRepository.createComment(dto.content, postId, user);
         if (!comment) throw new HttpException('', HttpStatus.NOT_FOUND);
-        console.log(comment);
         if (comment) {
             return {
                 id: comment.ID,
