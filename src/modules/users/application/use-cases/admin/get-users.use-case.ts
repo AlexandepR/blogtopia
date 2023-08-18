92fbc34591d629a8a59aa0a50e2077ea4d593ed6
Alex <alexandepp@gmail.com>
2023-08-18 17:31:56 +0300

    add typeorm repo for get users

--- src/modules/users/application/use-cases/admin/get-users.use-case.ts
+++ src/modules/users/application/use-cases/admin/get-users.use-case.ts
@@ -1,32 +1,41 @@
-import { PaginationType } from '../../../../../types/types';
-import { filterGetUsers, pagesCounter, parseQueryUsersPaginator, skipPage } from '../../../../../utils/helpers';
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { GetUsersOutputModelType, ParamsUsersType } from '../../../type/usersTypes';
-import { UsersSqlRepository } from '../../../infrastructure/users.sql-repository';
+import { PaginationType } from "../../../../../types/types";
+import { filterBanStatus, pagesCounter, parseQueryPaginator, skipPage } from "../../../../../utils/helpers";
+import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
+import { GetUsersOutputModelType, ParamsUsersType } from "../../../type/usersTypes";
+import { UsersOrmRepository } from "../../../infrastructure/users.orm-repository";
 
 
 export class GetUsersByAdminCommand {
-    constructor(public query: ParamsUsersType) {
-    }
+  constructor(public query: ParamsUsersType) {
+  }
 }
 
 @CommandHandler(GetUsersByAdminCommand)
 export class GetUsersByAdminUseCase implements ICommandHandler<GetUsersByAdminCommand> {
-    constructor(protected usersSqlRepository: UsersSqlRepository) {
-    }
-    async execute(command: GetUsersByAdminCommand): Promise<PaginationType<GetUsersOutputModelType[] | null>> {
-        const filter = filterGetUsers(command.query);
-        const { pageSize, pageNumber, sortDirection, sortBy } = parseQueryUsersPaginator(command.query);
-        const totalCountUsers = await this.usersSqlRepository.getTotalCountUsers(filter);
-        const skip = skipPage(pageNumber, pageSize);
-        const pagesCount = pagesCounter(totalCountUsers, pageSize);
-        const allUsers = await this.usersSqlRepository.getUsers(skip, pageSize, filter, sortBy, sortDirection);
-        return {
-            pagesCount: pagesCount,
-            page: pageNumber,
-            pageSize: pageSize,
-            totalCount: totalCountUsers,
-            items: allUsers
-        };
-    }
+  constructor(
+    protected UsersOrmRepository: UsersOrmRepository) {
+  }
+  async execute({ query }: GetUsersByAdminCommand): Promise<PaginationType<GetUsersOutputModelType[] | null>> {
+    const banStatus = filterBanStatus(query.banStatus)
+    const { pageSize, pageNumber, sortDirection, sortBy, searchLoginTerm, searchEmailTerm } = parseQueryPaginator(query);
+    const totalCountUsers = await this.UsersOrmRepository.getTotalCountUsers(banStatus, searchLoginTerm, searchEmailTerm);
+    const skip = skipPage(pageNumber, pageSize);
+    const pagesCount = pagesCounter(totalCountUsers, pageSize);
+    const allUsers = await this.UsersOrmRepository.getUsers(
+      skip,
+      pageSize,
+      sortBy,
+      sortDirection,
+      banStatus,
+      searchLoginTerm,
+      searchEmailTerm,
+    );
+    return {
+      pagesCount: pagesCount,
+      page: pageNumber,
+      pageSize: pageSize,
+      totalCount: totalCountUsers,
+      items: allUsers
+    };
+  }
 }
