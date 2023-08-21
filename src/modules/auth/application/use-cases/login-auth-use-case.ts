f9fa92e0988cae7574ad99f13e0b88e1fc967e90
Alex <alexandepp@gmail.com>
2023-08-21 17:09:11 +0300

    login use case to orm

--- src/modules/auth/application/use-cases/login-auth-use-case.ts
+++ src/modules/auth/application/use-cases/login-auth-use-case.ts
@@ -2,40 +2,38 @@ import { HttpException, HttpStatus, UnauthorizedException } from "@nestjs/common
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { isPasswordCorrect } from "../../../../utils/helpers";
 import { JwtService } from "../jwt.service";
-import { UsersRepository } from "../../../users/infrastructure/users.repository";
 import { validateOrRejectModel } from "../../../../utils/validation.helpers";
 import { loginInputClassModel } from "../../types/auth.types";
-import { UsersService } from "../../../users/application/users.service";
-import { SecurityService } from "../../../security/application/security.service";
-import { UsersSqlRepository } from '../../../users/infrastructure/users.sql-repository';
-import { SecuritySqlRepository } from '../../../security/infrastructure/security.sql-repository';
+import { UsersOrmRepository } from "../../../users/infrastructure/users.orm-repository";
+import { SecurityOrmRepository } from "../../../security/infrastructure/security.orm-repository";
 
 export class LoginAuthCommand {
   constructor(
     public signInDto: loginInputClassModel,
     public deviceName: string,
-    public ip: any,
-  ) {}
+    public ip: any
+  ) {
+  }
 }
 
 @CommandHandler(LoginAuthCommand)
-export class LoginAuthUseCase implements ICommandHandler<LoginAuthCommand>{
+export class LoginAuthUseCase implements ICommandHandler<LoginAuthCommand> {
   constructor(
     protected jwtService: JwtService,
-    protected usersSqlRepository: UsersSqlRepository,
-    protected securitySqlRepository: SecuritySqlRepository,
+    protected securityOrmRepository: SecurityOrmRepository,
+    protected usersOrmRepository: UsersOrmRepository
   ) {
   }
-  async execute(command: LoginAuthCommand) {
-    await validateOrRejectModel(command.signInDto, loginInputClassModel);
-    const user = await this.usersSqlRepository.findByLoginOrEmail(command.signInDto.loginOrEmail);
-    if(!user || user.banInfo.isBanned) throw new UnauthorizedException()
+  async execute({ signInDto, ip, deviceName, signInDto: { loginOrEmail, password } }: LoginAuthCommand) {
+    await validateOrRejectModel(signInDto, loginInputClassModel);
+    const user = await this.usersOrmRepository.findByLoginOrEmail(loginOrEmail);
+    if (!user || user.banInfo.isBanned) throw new UnauthorizedException();
     if (user) {
-      const isHash = await isPasswordCorrect(command.signInDto.password, user.passwordHash);
+      const isHash = await isPasswordCorrect(password, user.passwordHash);
       if (!user || !user.isConfirmed || !isHash) throw new HttpException("", HttpStatus.UNAUTHORIZED);
-      const createSession = await this.securitySqlRepository.createSession(user.ID, command.ip, command.deviceName);
+      const createSession = await this.securityOrmRepository.createSession(user.ID, ip, deviceName);
       if (!createSession) throw new HttpException("", HttpStatus.UNAUTHORIZED);
-      const token = await this.jwtService.сreateJWT(user);
+      const token = await this.jwtService.createJWT(user);
       const refreshToken = await this.jwtService.createRefreshToken(user, createSession.deviceId);
       const refreshTokenCookie = `refreshToken=${refreshToken}; HttpOnly; Secure`;
       // const refreshTokenCookie = `refreshToken=${refreshToken}`;
