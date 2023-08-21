6ad1cb4094c3dc82119296571a30beb71dbcdf68
Alex <alexandepp@gmail.com>
2023-08-21 17:10:20 +0300

    PasswordRecoveryAuth to orm

--- src/modules/auth/application/use-cases/passwordRecovery-auth-use-case.ts
+++ src/modules/auth/application/use-cases/passwordRecovery-auth-use-case.ts
@@ -1,11 +1,10 @@
 import { HttpException, HttpStatus } from "@nestjs/common";
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { EmailService } from "../../../mail/application/managers/email.service";
-import { UsersRepository } from "../../../users/infrastructure/users.repository";
 import { validateOrRejectModel } from "../../../../utils/validation.helpers";
 import { checkEmailInputClassModel } from "../../types/auth.types";
 import { v4 as uuidv4 } from "uuid";
-import { UsersSqlRepository } from '../../../users/infrastructure/users.sql-repository';
+import { UsersOrmRepository } from "../../../users/infrastructure/users.orm-repository";
 
 export class PasswordRecoveryAuthCommand {
   constructor(
@@ -17,15 +16,16 @@ export class PasswordRecoveryAuthCommand {
 @CommandHandler(PasswordRecoveryAuthCommand)
 export class PasswordRecoveryAuthUseCase implements ICommandHandler<PasswordRecoveryAuthCommand> {
   constructor(
-    protected usersSqlRepository: UsersSqlRepository,
+    protected usersOrmRepository: UsersOrmRepository,
     protected emailService: EmailService
-  ) {}
-  async execute(command: PasswordRecoveryAuthCommand) {
-    await validateOrRejectModel(command.dto, checkEmailInputClassModel);
+  ) {
+  }
+  async execute({ dto }: PasswordRecoveryAuthCommand) {
+    await validateOrRejectModel(dto, checkEmailInputClassModel);
     const newCode = uuidv4();
-    const updatePassRecoveryCode = await this.usersSqlRepository.updatePassRecoveryCode(command.dto.email, newCode);
+    const updatePassRecoveryCode = await this.usersOrmRepository.updatePassRecoveryCode(dto.email, newCode);
     if (!updatePassRecoveryCode) throw new HttpException("", HttpStatus.BAD_REQUEST);
-    const sendCode = await this.emailService.sendEmailRecoveryPassCode(command.dto.email, newCode);
+    const sendCode = await this.emailService.sendEmailRecoveryPassCode(dto.email, newCode);
     if (!sendCode) throw new HttpException("", HttpStatus.BAD_REQUEST);
     else throw new HttpException("", HttpStatus.NO_CONTENT);
   }
