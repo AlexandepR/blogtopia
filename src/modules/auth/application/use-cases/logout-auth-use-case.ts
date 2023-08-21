759a33cca4c073d99438976f263ed5ef3a6ac8ce
Alex <alexandepp@gmail.com>
2023-08-21 17:09:29 +0300

    logout to orm

--- src/modules/auth/application/use-cases/logout-auth-use-case.ts
+++ src/modules/auth/application/use-cases/logout-auth-use-case.ts
@@ -1,11 +1,10 @@
 import { HttpException, HttpStatus } from "@nestjs/common";
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { JwtService } from "../jwt.service";
-import { SecurityService } from "../../../security/application/security.service";
 import { Request } from "express";
 import * as jwt from "jsonwebtoken";
 import { settingsEnv } from "../../../../settings/settings";
-import { SecuritySqlRepository } from '../../../security/infrastructure/security.sql-repository';
+import { SecurityOrmRepository } from "../../../security/infrastructure/security.orm-repository";
 
 export class LogoutAuthCommand {
   constructor(
@@ -13,24 +12,25 @@ export class LogoutAuthCommand {
   ) {
   }
 }
+
 @CommandHandler(LogoutAuthCommand)
 export class LogoutAuthUseCase implements ICommandHandler<LogoutAuthCommand> {
   constructor(
     protected jwtService: JwtService,
-    protected securitySqlRepository: SecuritySqlRepository
+    protected securityOrmRepository: SecurityOrmRepository
   ) {
   }
-  async execute(command: LogoutAuthCommand) {
-    const refreshToken = command.req.cookies.refreshToken;
-    const user = command.req.requestUser;
+  async execute({ req }: LogoutAuthCommand) {
+    const refreshToken = req.cookies.refreshToken;
+    const user = req.requestUser;
     const getRefreshToken: any = jwt.verify(refreshToken, settingsEnv.JWT_REFRESH_TOKEN_SECRET);
-    const terminateSessions = await this.securitySqlRepository.terminateSessionByDeviceId(getRefreshToken.deviceId, user.ID);
+    const terminateSessions = await this.securityOrmRepository.terminateSessionByDeviceId(getRefreshToken.deviceId, user.ID);
     if (terminateSessions) {
       const expiredRefreshToken = await this.jwtService.refreshTokenToDeprecated(user, refreshToken);
-      throw new HttpException("", HttpStatus.NO_CONTENT);
       if (!expiredRefreshToken) {
-        throw new HttpException("", HttpStatus.NO_CONTENT);
+        throw new HttpException("", HttpStatus.BAD_REQUEST);
       }
     }
+    return new HttpException("", HttpStatus.NO_CONTENT)
   }
 }
