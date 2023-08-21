0c078eb85994ac97066e74062a02ed51e34eec04
Alex <alexandepp@gmail.com>
2023-08-21 17:09:55 +0300

    devices to orm

--- src/modules/auth/application/use-cases/me-auth-use-case.ts
+++ src/modules/auth/application/use-cases/me-auth-use-case.ts
@@ -1,8 +1,7 @@
 import { HttpException, HttpStatus } from "@nestjs/common";
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { Request } from "express";
-import { UsersRepository } from "../../../users/infrastructure/users.repository";
-import { UsersSqlRepository } from '../../../users/infrastructure/users.sql-repository';
+import { UsersOrmRepository } from "../../../users/infrastructure/users.orm-repository";
 
 export class getOwnAccountAuthCommand {
   constructor(
@@ -10,22 +9,23 @@ export class getOwnAccountAuthCommand {
   ) {
   }
 }
+
 @CommandHandler(getOwnAccountAuthCommand)
 export class getOwnAccountAuthUseCase implements ICommandHandler<getOwnAccountAuthCommand> {
   constructor(
-    protected usersSqlRepository: UsersSqlRepository
+    protected usersOrmRepository: UsersOrmRepository,
   ) {
   }
-  async execute(command: getOwnAccountAuthCommand) {
-    const userId = command.req.requestUser.ID;
-    if(!userId) throw new HttpException("", HttpStatus.UNAUTHORIZED);
-    const findUser = await this.usersSqlRepository.findUserById(userId);
-    if(!findUser) throw new HttpException("", HttpStatus.UNAUTHORIZED);
+  async execute({ req }: getOwnAccountAuthCommand) {
+    const userId = req.requestUser.ID;
+    if (!userId) throw new HttpException("", HttpStatus.UNAUTHORIZED);
+    const findUser = await this.usersOrmRepository.findUserById(userId);
+    if (!findUser) throw new HttpException("", HttpStatus.UNAUTHORIZED);
     if (findUser) {
-      return{
-        'email': findUser.email,
-        'login': findUser.login,
-        'userId': findUser.ID,
+      return {
+        "email": findUser.email,
+        "login": findUser.login,
+        "userId": findUser.ID
       };
     } else {
       throw new HttpException("", HttpStatus.BAD_REQUEST);
