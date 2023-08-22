a8a2f05d965bff53f8ac3af21a94e409c34b551c
Alex <alexandepp@gmail.com>
2023-08-22 21:07:49 +0300

    refactor public blog controller

--- src/modules/blogs/api/public.api.blog.controller.ts
+++ src/modules/blogs/api/public.api.blog.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Param, Query, Req } from "@nestjs/common";
-import { Request } from "express";
+import { Controller, Get, Param, Query, Req } from '@nestjs/common';
+import { Request } from 'express';
 
-import { CommandBus } from "@nestjs/cqrs";
-import { GetBlogCommand } from "../application/use-cases/public/getBlog-blog-public-use-case";
-import { GetBlogsCommand } from "../application/use-cases/public/get-blogs-public-use-case";
-import { GetPostsByBlogCommand } from "../application/use-cases/public/getPostsByblogs-public-use-case";
-import { Public } from "../../../utils/public.decorator";
-import { ParamsType, ParamsTypeClassModel } from "../../../types/types";
+import { CommandBus } from '@nestjs/cqrs';
+import { GetBlogCommand } from '../application/use-cases/public/getBlog-blog-public-use-case';
+import { GetBlogsCommand } from '../application/use-cases/public/get-blogs-public-use-case';
+import { GetPostsByBlogCommand } from '../application/use-cases/public/getPostsByblogs-public-use-case';
+import { Public, UserFromRequestDecorator } from '../../../utils/public.decorator';
+import { ParamsType } from '../../../types/types';
+import { UserType } from '../../users/type/usersTypes';
 
 
 @Controller("blogs")
@@ -18,7 +19,7 @@ export class BlogsPublicController {
   @Public()
   @Get()
   async getBlogs(
-    @Query() query: ParamsTypeClassModel,
+    @Query() query: ParamsType,
   ) {
     const command = new GetBlogsCommand(query)
     return await this.commandBus.execute(command);
@@ -35,12 +36,12 @@ export class BlogsPublicController {
   @Public()
   @Get(":id/posts")
   async GetPostsByBlog(
-    @Req() req:Request,
+    @UserFromRequestDecorator() user: UserType,
     @Param("id")
       id: string,
     @Query() query: ParamsType
   ) {
-    const command = new GetPostsByBlogCommand(id, query)
+    const command = new GetPostsByBlogCommand(id, query, user)
     return await this.commandBus.execute(command);
   }
 }
