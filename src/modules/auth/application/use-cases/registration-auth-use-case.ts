c90aef933e0c82b9c9e5a10e989f0b5ccdac048c
Alex <alexandepp@gmail.com>
2023-08-22 21:08:32 +0300

    refactor auth case to orm

--- src/modules/auth/application/use-cases/registration-auth-use-case.ts
+++ src/modules/auth/application/use-cases/registration-auth-use-case.ts
@@ -1,44 +1,40 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { CreateUserInputClassModel } from '../../../users/type/usersTypes';
-import { generateHash } from '../../../../utils/helpers';
-import { JwtService } from '../jwt.service';
-import { EmailService } from '../../../mail/application/managers/email.service';
-import { UsersRepository } from '../../../users/infrastructure/users.repository';
-import { validateOrRejectModel } from '../../../../utils/validation.helpers';
-import { UsersSqlRepository } from '../../../users/infrastructure/users.sql-repository';
+import { HttpException, HttpStatus } from "@nestjs/common";
+import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
+import { CreateUserInputClassModel } from "../../../users/type/usersTypes";
+import { generateHash } from "../../../../utils/helpers";
+import { EmailService } from "../../../mail/application/managers/email.service";
+import { validateOrRejectModel } from "../../../../utils/validation.helpers";
+import { UsersOrmRepository } from "../../../users/infrastructure/users.orm-repository";
 
 export class RegistrationAuthCommand {
-    constructor(
-        public dto: CreateUserInputClassModel,
-        public ip: any
-    ) {
-    }
+  constructor(
+    public dto: CreateUserInputClassModel,
+    public ip: any
+  ) {
+  }
 }
 
 @CommandHandler(RegistrationAuthCommand)
 export class RegistrationAuthUseCase implements ICommandHandler<RegistrationAuthCommand> {
-    constructor(
-        protected jwtService: JwtService,
-        protected emailService: EmailService,
-        protected usersRepository: UsersRepository,
-        protected usersSqlRepository: UsersSqlRepository
-    ) {
+  constructor(
+    protected emailService: EmailService,
+    protected usersOrmRepository: UsersOrmRepository
+  ) {
+  }
+  async execute({ dto, ip, dto: { email, login, password } }: RegistrationAuthCommand) {
+    await validateOrRejectModel(dto, CreateUserInputClassModel);
+    const passwordHash = await generateHash(password);
+    const confirmEmail = false;
+    const findUserByEmail = await this.usersOrmRepository.findByLoginOrEmail(email);
+    const findUserByLogin = await this.usersOrmRepository.findByLoginOrEmail(login);
+    if (findUserByLogin || findUserByEmail) throw new HttpException("", HttpStatus.BAD_REQUEST);
+    const user = await this.usersOrmRepository.createUser(dto, passwordHash, ip, confirmEmail);
+    try {
+      await this.emailService.sendEmailConfirmationMessage(user);
+    } catch (error) {
+      await this.usersOrmRepository.deleteUser(user.id);
+      throw new HttpException("", HttpStatus.BAD_REQUEST);
     }
-    async execute(command: RegistrationAuthCommand) {
-        await validateOrRejectModel(command.dto, CreateUserInputClassModel);
-        const passwordHash = await generateHash(command.dto.password);
-        const confirmEmail = false;
-        const findUserByEmail = await this.usersSqlRepository.findByLoginOrEmail(command.dto.email);
-        const findUserByLogin = await this.usersSqlRepository.findByLoginOrEmail(command.dto.login);
-        if (findUserByLogin || findUserByEmail) throw new HttpException('', HttpStatus.BAD_REQUEST);
-        const user = await this.usersSqlRepository.createUser(command.dto, passwordHash, command.ip, confirmEmail);
-        try {
-            await this.emailService.sendEmailConfirmationMessage(user);
-        } catch (error) {
-                await this.usersSqlRepository.deleteUser(user.id);
-            throw new HttpException('', HttpStatus.BAD_REQUEST);
-        }
-        throw new HttpException('', HttpStatus.NO_CONTENT);
-    }
+    throw new HttpException("", HttpStatus.NO_CONTENT);
+  }
 }
