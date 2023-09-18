b9317448d4462b53a6e440209844018f031dd97a
Alex <alexandepp@gmail.com>
2023-09-19 02:39:14 +0300

    change user Entity

--- src/modules/users/domain/entities/user.entity.ts
+++ src/modules/users/domain/entities/user.entity.ts
@@ -1,41 +1,57 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { Blogs } from '../../../blogs/domain/entities/blogs.entity';
+import { Column, Entity, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Blogs } from "../../../blogs/domain/entities/blogs.entity";
+import { BanUserInfo } from "./banUser.entity";
+import { UsersDevicesSessions } from "./usersDevices.entity";
 
-@Entity('Users')
+@Entity("Users")
 export class Users {
-    @PrimaryGeneratedColumn('uuid')
-    ID: string;
 
-    @Column({ collation: 'C' })
-    login: string;
+  @PrimaryGeneratedColumn("uuid")
+  ID: string;
 
-    @Column({ collation: 'C' })
-    email: string;
+  @Column({
+    type: "character varying",
+    length: 15,
+    collation: "C"
+  })
+  login: string;
 
-    @Column({ type: 'timestamp with time zone' })
-    created_at: Date;
+  @Column({
+    type: "varchar",
+    collation: "C"
+  })
+  email: string;
 
-    @Column({ default: ""})
-    passwordHash: string;
+  // @Column({ type: "varchar" })
+  @Column({ default: new Date(), type: "timestamp with time zone" })
+  createdAt: Date;
+  // createdAt: string;
 
-    @Column({ default: false })
-    isConfirmed: boolean;
+  @Column({ default: "" })
+  passwordHash: string;
 
-    @Column({ default: ""})
-    confirmationCode: string;
+  @Column({ default: false })
+  isConfirmed: boolean;
 
-    @Column({ default: ""})
-    expConfirmCodeDate: string;
+  @Column({ default: "" })
+  confirmationCode: string;
 
-    @Column({ default: ""})
-    passRecoveryCode: string;
+  @Column({ default: null })
+  expConfirmCodeDate: Date;
 
-    @Column('date', { array: true, default: [] })
-    sendEmails: string[];
+  @Column({ default: "" })
+  passRecoveryCode: string;
 
-    @Column('varchar', { array: true, default: [] })
-    expRefreshToken: string[];
+  @Column("date", { array: true, default: [] })
+  sendEmails: string[];
 
-    @OneToMany(() => Blogs, (blog) => blog.user)
-    blogs: Blogs[];
+  @Column("varchar", { array: true, default: [] })
+  expRefreshToken: string[];
+
+  @OneToMany(() => Blogs, (blog) => blog.user)
+  blogs: Blogs[];
+  @OneToOne(() => BanUserInfo, banInfo => banInfo.user)
+  banInfo: BanUserInfo;
+  @OneToMany(() => UsersDevicesSessions, devicesSessions => devicesSessions.user)
+  devicesSessions: UsersDevicesSessions[];
 }
