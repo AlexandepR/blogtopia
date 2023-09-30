1827144052cbdb07a65a84b18c66f70ed85fc09e
Alex <alexandepp@gmail.com>
2023-09-30 03:37:10 +0300

    change get blogs

--- src/modules/blogs/application/use-cases/admin/get-blogs.use-case.ts
+++ src/modules/blogs/application/use-cases/admin/get-blogs.use-case.ts
@@ -1,12 +1,8 @@
 import { PaginationType, ParamsType } from "../../../../../types/types";
-import { UserDocument } from "../../../../users/domain/entities/users.schema";
-import { BlogDocument } from "../../../domain/entities/blogs.schema";
-import { filterByNameTermOrUserLogin, pagesCounter, parseQueryPaginator, skipPage } from "../../../../../utils/helpers";
-import { BlogsRepository } from "../../../infrastructure/blogs.repository";
-import { Injectable } from "@nestjs/common";
-import { CreatePostInputClassModel } from "../../../../posts/type/postsType";
+import { pagesCounter, parseQueryPaginator, skipPage } from "../../../../../utils/helpers";
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
-import { BlogsQueryRepository } from "../../../infrastructure/blogs.query-repository";
+import { BlogType } from "../../../type/blogsType";
+import { BlogsOrmQueryRepository } from "../../../infrastructure/blogs.orm.query-repository";
 
 
 export class getBlogsByAdminCommand {
@@ -18,17 +14,16 @@ export class getBlogsByAdminCommand {
 @CommandHandler(getBlogsByAdminCommand)
 export class GetBlogsByAdminUseCase implements ICommandHandler<getBlogsByAdminCommand>{
   constructor(
-    protected blogsRepository: BlogsRepository,
-    protected blogsQueryRepository: BlogsQueryRepository,
+    protected blogsOrmQueryRepository: BlogsOrmQueryRepository
     ) {
   }
-  async execute(command: getBlogsByAdminCommand): Promise<PaginationType<BlogDocument[]>> {
+  async execute(command: getBlogsByAdminCommand): Promise<PaginationType<BlogType[] | any> | any> {
     const { searchNameTerm, pageSize, pageNumber, sortDirection, sortBy } = parseQueryPaginator(command.query);
-    const filter = searchNameTerm ? { name: { $regex: searchNameTerm, $options: "i" } } : {};
-    const getTotalCountBlogs = await this.blogsQueryRepository.getTotalCountBlogs(filter);
+    const getTotalCountBlogs = await this.blogsOrmQueryRepository.getTotalBlogsByAdmin(searchNameTerm);
     const skip = skipPage(pageNumber, pageSize);
     const pagesCount = pagesCounter(getTotalCountBlogs, pageSize);
-    const getBlogs = await this.blogsQueryRepository.getBlogs(skip, pageSize, filter, sortBy, sortDirection, true);
+    const getBlogs = await this.blogsOrmQueryRepository
+      .getBlogsByAdmin(skip, pageSize, searchNameTerm, sortBy, sortDirection);
     return {
       pagesCount: pagesCount,
       page: pageNumber,
