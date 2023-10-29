c25b2de974cccbc592daddbc722d7c683b644374
Alex <alexandepp@gmail.com>
2023-10-29 20:06:10 +0300

    change registration use case to orm

--- src/modules/auth/application/use-cases/registrationConfirmation-auth-use-case.ts
+++ src/modules/auth/application/use-cases/registrationConfirmation-auth-use-case.ts
@@ -1,11 +1,9 @@
 import { BadRequestException, HttpException, HttpStatus } from "@nestjs/common";
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { updateConfirmInfo } from "../../../../utils/helpers";
-import { UsersRepository } from "../../../users/infrastructure/users.repository";
 import { validateOrRejectModel } from "../../../../utils/validation.helpers";
 import { codeInputClassModel } from "../../types/auth.types";
-import { UsersService } from "../../../users/application/users.service";
-import { UsersSqlRepository } from '../../../users/infrastructure/users.sql-repository';
+import { UsersOrmRepository } from "../../../users/infrastructure/users.orm-repository";
 
 export class ConfirmRegistrationAuthCommand {
   constructor(
@@ -16,24 +14,22 @@ export class ConfirmRegistrationAuthCommand {
 @CommandHandler(ConfirmRegistrationAuthCommand)
 export class ConfirmRegistrationAuthUseCase implements ICommandHandler<ConfirmRegistrationAuthCommand> {
   constructor(
-    protected usersRepository: UsersRepository,
-    protected usersService: UsersService,
-    protected UsersSqlRepository: UsersSqlRepository,
+    protected usersOrmRepository: UsersOrmRepository,
   ) {
   }
   async execute(command: ConfirmRegistrationAuthCommand) {
     await validateOrRejectModel(command.dto, codeInputClassModel);
-    const user = await this.UsersSqlRepository.findByConfirmationCode(command.dto.code);
+    const user = await this.usersOrmRepository.findByConfirmationCode(command.dto.code);
     if (!user || user.isConfirmed === true) throw new BadRequestException();
     const expConfirmCodeDate = new Date(user.expConfirmCodeDate)
     const dateNow = new Date();
     if (expConfirmCodeDate.getTime() - dateNow.getTime() <= 0) {
-      await this.UsersSqlRepository.deleteUser(user.ID);
+      await this.usersOrmRepository.deleteUser(user.ID);
       throw new HttpException("", HttpStatus.BAD_REQUEST);
     }
     if (updateConfirmInfo(user, command.dto.code)) {
       const isConfirmed = user.isConfirmed = true;
-      const createUser = await this.UsersSqlRepository.updateConfirmStatus(user.ID, isConfirmed)
+      const createUser = await this.usersOrmRepository.updateConfirmStatus(user.ID, isConfirmed)
       if (createUser) throw new HttpException("", HttpStatus.NO_CONTENT);
     }
     throw new HttpException("", HttpStatus.BAD_REQUEST);
